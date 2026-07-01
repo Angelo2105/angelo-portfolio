cb0facebe4697432643bd3e1e9c63b17830876d6
Angelo2105 <angelor@clemson.edu>
2026-07-01 13:10:11 -0400

    view projects button established

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("helloBtn");
+const projectsButton = document.getElementById("projectsButton");
 
 button.addEventListener("click", () => {
     alert("Welcome to Angelo's Portfolio!");
